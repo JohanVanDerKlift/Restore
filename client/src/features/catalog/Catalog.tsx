fe4b733c4f7211a906bf826bca0d9fc9fe14f60a
JohanVanDerKlift <info@johanvanderklift.nl>
2025-12-16 17:52:29 +0100

    End of section 3

---
+++ client/src/features/catalog/Catalog.tsx
@@ -0,0 +1,14 @@
+import type {Product} from "../../app/models/product.ts";
+import ProductList from "./ProductList.tsx";
+
+type Props = {
+    products: Product[]
+}
+
+export default function Catalog({products}: Props) {
+    return (
+        <>
+            <ProductList products={products} />
+        </>
+    );
+}
