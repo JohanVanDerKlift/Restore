faab1bbfb6bd565151df8b1b2809e9479036aeec
JohanVanDerKlift <info@johanvanderklift.nl>
2026-01-07 16:59:15 +0100

    End of section 6

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -9,10 +9,13 @@ import { RouterProvider } from 'react-router';
 import { router } from "./app/layout/routes/Routes.tsx";
 import {Provider} from "react-redux";
 import {store} from "./app/store/store.ts";
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
       <Provider store={store}>
+          <ToastContainer position='bottom-left' hideProgressBar theme='colored' />
           <RouterProvider router={router} />
       </Provider>
   </StrictMode>
