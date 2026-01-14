a56540c6d62a819ca837d6bccce9c42cc5e0abfa
JohanVanDerKlift <info@johanvanderklift.nl>
2026-01-14 18:01:00 +0100

    end of section 8

--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -1,14 +1,40 @@
 import ProductList from "./ProductList.tsx";
-import {useFetchProductsQuery} from "./catalogApi.ts";
+import {useFetchFiltersQuery, useFetchProductsQuery} from "./catalogApi.ts";
+import {Grid2, Typography} from "@mui/material";
+import FIlters from "./Filters.tsx";
+import {useAppDispatch, useAppSelector} from "../../app/store/store.ts";
+import AppPagination from "../../app/shared/components/AppPagination.tsx";
+import {setPageNumber} from "./catalogSlice.ts";
 
 export default function Catalog() {
-    const {data, isLoading} = useFetchProductsQuery();
+    const productParams = useAppSelector(state => state.catalog);
+    const {data, isLoading} = useFetchProductsQuery(productParams);
+    const {data: filtersData, isLoading: filtersLoading} = useFetchFiltersQuery();
+    const dispatch = useAppDispatch();
 
-    if (isLoading || !data) return <div>Loading...</div>
+    if (isLoading || !data || filtersLoading || !filtersData) return <div>Loading...</div>
 
     return (
-        <>
-            <ProductList products={data} />
-        </>
+        <Grid2 container spacing={4}>
+            <Grid2 size={3}>
+                <FIlters filtersData={filtersData}/>
+            </Grid2>
+            <Grid2 size={9}>
+                {data.items && data.items.length > 0 ? (
+                    <>
+                        <ProductList products={data.items} />
+                        <AppPagination
+                            metadata={data.pagination}
+                            onPageChange={(page: number)=> {
+                                dispatch(setPageNumber(page));
+                                window.scrollTo({top: 0, behavior: 'smooth'})
+                            }}
+                        />
+                    </>
+                ) : (
+                    <Typography variant="h5">There are no results for this filter</Typography>
+                )}
+            </Grid2>
+        </Grid2>
     );
 }
